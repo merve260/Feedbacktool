f968294b1e93e5f660e2ca14acc7c2c6dabf729c
Merve Koc <mk@plazz.ag>
2025-10-09 10:05:04 +0200

    fehler-beheben-englisch

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,4 @@ set -e  # Script sofort beenden, falls ein Fehler auftritt
 # Produktion-Build erstellen
 ng build --configuration production
 
-# Deployment zu Firebase Hosting durchführen
-firebase deploy --only hosting
 
